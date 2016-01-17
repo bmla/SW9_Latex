46d9f9181627479f7adc545b709b05181e2c5974
Elias <eliasringhauge@hotmail.com>
2016-01-17 18:32:14 +0100

    Alternate backpage version

---
+++ BuildAll_backpage.bat
@@ -0,0 +1,22 @@
+@echo off
+:: This file is explicitly made for Dat_p9 project with the goal of
+:: compiling the litterature, the paper and this folder's main
+:: tex files ordered, so the result becomes one pdf with the two
+:: papers inside.
+
+
+
+Echo Building Litterature
+START "LitBuilder" /D "%~dp0review" /wait  "BuildLiterature.bat" -nonstop 
+
+Echo Building Paper
+START "PaperBuilder" /D "%~dp0paper" /wait "BuildPaper.bat" -nonstop
+
+:: Echo Building Appendix
+:: START "AppendixBuilder" /D "%~dp0appendix" /wait "BuildAppendix.bat" -nonstop
+
+
+Echo Building Combined PDF
+START "CombinedBuilder"  /D "%~dp0" /wait "BuildCombined_backpage.bat" -nonstop
+
+ECHO Done
---
+++ BuildCombined_backpage.bat
@@ -0,0 +1,72 @@
+﻿::et script til at compile pdf filer, fra main.tex til main-%date% (system datoen
+:: på dit system) der er også et antal linjer som sletter alle de små filer
+:: pdflatex skaber når den bliver kørt.
+:: Filen kan sættes til at køre sletningen alene, eller til at beholde filerne.
+:: TODO:
+:: --help/-h/-? kommandoer dog indeholder filen stadig kun meget lidt og skulle være til at læse så problemet er ikke stort.
+:: tjek om main.tex faktisk findes...
+:: brug andre filer -f/--file
+@echo off
+
+:: Set Local Variables
+SETLOCAL
+SET _filename=report_backpage.tex
+::SET _target=main-%date%
+SET _target=report_backpage
+
+del /F /Q "report_backpage.pdf"
+
+if "%1" == "-c" goto :cleanup
+if "%1" == "--cleanup" goto :cleanup
+
+if "%1" == "-nonstop" goto :nonstop
+
+pdflatex %_filename% -job-name=%_target%
+biber %_target%
+pdflatex %_filename% -job-name=%_target%
+pdflatex %_filename% -job-name=%_target%
+
+if "%1" == "-nc" goto :END
+if "%1" == "--nocleanup" goto :END
+goto :cleanup
+
+:nonstop
+pdflatex %_filename% -job-name=%_target% -interaction=nonstopmode
+biber %_target%
+pdflatex %_filename% -job-name=%_target% -interaction=nonstopmode
+pdflatex %_filename% -job-name=%_target% -interaction=nonstopmode
+
+if "%2" == "-nc" goto :END
+if "%2" == "--nocleanup" goto :END
+
+:cleanup
+del /F *.out
+del /F *.aux
+del /F *.bbl
+del /F *.blg
+del /F *.txt
+del /F *.synctex
+del /F *.toc
+del /F *.log
+del /F *.gz
+del /F *.gz(busy)
+
+del /S "*.bbl"
+del /S "*.blg"
+del /S "*.log"
+del /S "*.out"
+del /S "*.toc"
+del /S "*.aux"
+del /S "*.lox"
+del /S "*.tps"
+del /S "*.tdo"
+del /S "*.synctex.gz"
+del /S "*.gz*"
+del /S "*.bcf"
+del "*.xml"
+
+
+
+:END
+ENDLOCAL
+exit
